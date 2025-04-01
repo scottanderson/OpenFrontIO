73862230be5aad7b18e122b1cd4ab05fc9570b2c
Scott Anderson <662325+scottanderson@users.noreply.github.com>
2025-04-01 01:33:09 -0400

    ESLint github workflow and webpack plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,18 @@
+import { includeIgnoreFile } from "@eslint/compat";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import tseslint from "typescript-eslint";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
+  includeIgnoreFile(gitignorePath),
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
